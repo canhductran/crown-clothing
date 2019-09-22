be6894e4d5cf933044b46819589eb383f83eae8b
Canh Duc Tran <CHRIS@Canhs-MBP.modem>
2019-09-22 19:09:34 +1000

    Added Sign up feature

--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -12,6 +12,33 @@ const config = {
     appId: "1:9964207514:web:d36a83f7507f004ed1ce23"
 };
 
+export const createUserProfileDocument = async(userAuth, additionalData) => {
+    if (!userAuth) {
+        return;
+    }
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const {displayName, email} = userAuth;
+        const createdDate = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdDate,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
+}
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
