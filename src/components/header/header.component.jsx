c98b283db5ef653bf867b6eea0c31011b661d402
CHRIS <chris.canhductran@gmail.com>
2020-01-17 13:13:13 +1100

    added redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import {Link} from 'react-router-dom';
 
+import {connect} from 'react-redux';
+
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 
 import {auth} from '../../firebase/firebase.util';
@@ -30,4 +32,8 @@ const Header = ({currentUser}) => (
     </div>
 );
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentuser
+});
+
+export default connect(mapStateToProps)(Header);
