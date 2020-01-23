49ae28d13595f565ae3bff71ed5c6517869dece9
CHRIS <chris.canhductran@gmail.com>
2020-01-23 13:59:17 +1100

    refactored some shit

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
 import './collections-overview.styles.scss';
-import {selectShopItems} from '../../redux/shop/shop.selectors';
+import {selectCollectionsForPreview} from '../../redux/shop/shop.selectors';
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
 const CollectionsOverview = ({collections}) => (
@@ -16,7 +16,7 @@ const CollectionsOverview = ({collections}) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectShopItems
+    collections: selectCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -2,7 +2,18 @@ import {createSelector} from 'reselect';
 
 const shopSelector = state => state.shop;
 
-export const selectShopItems = createSelector(
+export const selectCollections = createSelector(
   [shopSelector],
-  shop => shop.shopItems
+  shop => shop.collections
 );
+
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  collections => Object.keys(collections).map(key => collections[key])
+)
+
+export const selectCollection = collectionUrlParam =>
+createSelector(
+  [selectCollections],
+  collections => collections[collectionUrlParam]
+)
