413e5e67b549e9062353306bb89d3bf735762a5c
CHRIS <chris.canhductran@gmail.com>
2020-01-23 15:07:27 +1100

    Added missing contact page

---
+++ src/pages/contact/contact.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ContactPage = () => (
+  <div>
+    <h1>Contact</h1>
+    <h2>Email: chris.canhductran@gmail.com</h2>
+  </div>
+);
+
+export default ContactPage;
