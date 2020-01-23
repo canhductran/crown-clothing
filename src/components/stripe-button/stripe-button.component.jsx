13c460af2b0ffef07062f4149c4a43b791987859
CHRIS <chris.canhductran@gmail.com>
2020-01-23 14:24:02 +1100

    added stripe

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({price}) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_8Qk7usJ6miL0oAGzh3DnHUtd00S31yphV0';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Successful')
+  }
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  )
+}
+
+export default StripeCheckoutButton;
